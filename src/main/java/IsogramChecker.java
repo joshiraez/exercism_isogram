ccb89c7d8c88220a2ad3ad6ff3fababbde0875fe
JoshiRaez <joseraezrodriguez@gmail.com>
2020-01-23 01:46:54 +0100

    Style cleanup

--- src/main/java/IsogramChecker.java
+++ src/main/java/IsogramChecker.java
@@ -5,17 +5,17 @@ class IsogramChecker {
         final String phraseNormalized = removeHyphensAndSpaces(phrase);
 
         return isTheSameLengthAfterRemovingDuplicatedLetters(phraseNormalized);
-
-
     }
 
     private boolean isTheSameLengthAfterRemovingDuplicatedLetters(final String phraseNormalized) {
+
         return phraseNormalized.chars()
                 .distinct()
                 .count() == phraseNormalized.length();
     }
 
     private String removeHyphensAndSpaces(final String phrase) {
+
         return phrase.toLowerCase()
                 .replace("-", "")
                 .replace(" ", "");
